3478575ba313538510e43c08a1096ae05f4db89f
Basti <89860334+Xerolux@users.noreply.github.com>
2025-04-18 18:48:24 +0200

    Update openwb-install.sh

--- openwb-install.sh
+++ openwb-install.sh
@@ -6,11 +6,11 @@ VENV_DIR="${OPENWBBASEDIR}/venv"
 
 # Prüfen, ob das Script als Root ausgeführt wird
 if (( $(id -u) != 0 )); then
-    echo "Dieses Script muss als Benutzer Root oder mit sudo ausgeführt werden"
+    echo "this script has to be run as user root or with sudo"
     exit 1
 fi
 
-echo "Installiere openWB 2 in \"${OPENWBBASEDIR}\""
+echo "installing openWB 2 into \"${OPENWBBASEDIR}\""
 
 # Debian-Version oder Codename erkennen
 DEBIAN_VERSION="unknown"
@@ -78,11 +78,11 @@ fi
 
 echo "Erkannte Debian-Version: $DEBIAN_VERSION (Codename: $DEBIAN_CODENAME)"
 
-# Setze USE_VENV basierend auf der Debian-Version
-if [[ "$DEBIAN_VERSION" == "12" || "$DEBIAN_VERSION" == "13" || "$DEBIAN_VERSION" == "unstable" ]]; then
-    USE_VENV=true
-else
-    USE_VENV=false
+# Installationspakete über ein aktualisiertes Script installieren
+curl -s "https://raw.githubusercontent.com/Xerolux/OpenWB2-Bookworm-Trixie/master/runs/install_packages.sh" | bash -s
+if [ $? -ne 0 ]; then
+    echo "Versuche lokales install_packages.sh..."
+    bash ./install_packages.sh
 fi
 
 # Installiere zusätzliche Build-Tools für Debian 12, 13 und unstable
@@ -91,6 +91,13 @@ if [[ "$DEBIAN_VERSION" == "12" || "$DEBIAN_VERSION" == "13" || "$DEBIAN_VERSION
     apt-get update
     apt-get install -y autoconf automake build-essential libtool
     echo "Zusätzliche Build-Tools erfolgreich installiert."
+fi
+
+# Installiere libxml2 und libxslt für Debian 12, 13, 14 und höher
+if [[ "$DEBIAN_VERSION" =~ ^[0-9]+$ ]] && [[ "$DEBIAN_VERSION" -ge 12 ]]; then
+    echo "Installiere libxml2 und libxslt für Debian $DEBIAN_VERSION..."
+    apt-get install -y libxml2 libxslt1.1
+    echo "libxml2 und libxslt erfolgreich installiert."
 fi
 
 # Installiere notwendige Netzwerk- und Firewall-Pakete
@@ -121,11 +128,126 @@ if [[ "$DEBIAN_VERSION" == "12" || "$DEBIAN_VERSION" == "13" || "$DEBIAN_VERSION
     show_warning
 fi
 
-# Installationspakete über ein aktualisiertes Script installieren
-curl -s "https://raw.githubusercontent.com/Xerolux/OpenWB2-Bookworm-Trixie/master/runs/install_packages.sh" | bash -s
-if [ $? -ne 0 ]; then
-    echo "Versuche lokales install_packages.sh..."
-    bash ./install_packages.sh
+echo "create group $OPENWB_GROUP"
+# Will do nothing if group already exists:
+/usr/sbin/groupadd "$OPENWB_GROUP"
+echo "done"
+
+echo "create user $OPENWB_USER"
+# Will do nothing if user already exists:
+/usr/sbin/useradd "$OPENWB_USER" -g "$OPENWB_GROUP" --create-home
+echo "done"
+
+# Sudo-Rechte für den Benutzer hinzufügen
+echo "$OPENWB_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/openwb
+chmod 440 /etc/sudoers.d/openwb
+echo "done"
+
+echo "check for initial git clone..."
+if [ ! -d "${OPENWBBASEDIR}/web" ]; then
+    mkdir -p "$OPENWBBASEDIR"
+    chown "$OPENWB_USER:$OPENWB_GROUP" "$OPENWBBASEDIR"
+    sudo -u "$OPENWB_USER" git clone https://github.com/Xerolux/OpenWB2-Bookworm-Trixie.git --branch master "$OPENWBBASEDIR"
+    echo "git cloned"
+else
+    echo "ok"
+fi
+
+echo -n "check for ramdisk... "
+if grep -Fq "tmpfs ${OPENWBBASEDIR}/ramdisk" /etc/fstab; then
+    echo "ok"
+else
+    mkdir -p "${OPENWBBASEDIR}/ramdisk"
+    sudo tee -a "/etc/fstab" <"${OPENWBBASEDIR}/data/config/ramdisk_config.txt" >/dev/null
+    mount -a
+    echo "created"
+fi
+
+echo -n "check for crontab... "
+if [ ! -f /etc/cron.d/openwb ]; then
+    cp "${OPENWBBASEDIR}/data/config/openwb.cron" /etc/cron.d/openwb
+    echo "installed"
+else
+    echo "ok"
+fi
+
+# check for mosquitto configuration
+echo "updating mosquitto config file"
+systemctl stop mosquitto
+sleep 2
+cp -a "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto.conf" /etc/mosquitto/mosquitto.conf
+mkdir -p /etc/mosquitto/conf.d
+cp "${OPENWBBASEDIR}/data/config/mosquitto/openwb.conf" /etc/mosquitto/conf.d/openwb.conf
+cp "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto.acl" /etc/mosquitto/mosquitto.acl
+sudo cp /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/mosquitto/certs/openwb.pem
+sudo cp /etc/ssl/private/ssl-cert-snakeoil.key /etc/mosquitto/certs/openwb.key
+sudo chgrp mosquitto /etc/mosquitto/certs/openwb.key
+systemctl start mosquitto
+
+# check for mosquitto_local instance
+if [ ! -f /etc/init.d/mosquitto_local ]; then
+    echo "setting up mosquitto local instance"
+    install -d -m 0755 -o root -g root /etc/mosquitto/conf_local.d/
+    install -d -m 0755 -o mosquitto -g root /var/lib/mosquitto_local
+    cp "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto_local_init" /etc/init.d/mosquitto_local
+    chown root:root /etc/init.d/mosquitto_local
+    chmod 755 /etc/init.d/mosquitto_local
+    systemctl daemon-reload
+    systemctl enable mosquitto_local
+else
+    systemctl stop mosquitto_local
+    sleep 2
+fi
+cp -a "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto_local.conf" /etc/mosquitto/mosquitto_local.conf
+cp -a "${OPENWBBASEDIR}/data/config/mosquitto/openwb_local.conf" /etc/mosquitto/conf_local.d/
+systemctl start mosquitto_local
+echo "mosquitto done"
+
+# apache
+echo -n "replacing apache default page..."
+cp "${OPENWBBASEDIR}/data/config/apache/000-default.conf" "/etc/apache2/sites-available/"
+cp "${OPENWBBASEDIR}/index.html" /var/www/html/index.html
+echo "done"
+echo -n "fix upload limit..."
+if [ -d "/etc/php/7.3/" ]; then
+    echo "upload_max_filesize = 300M" > /etc/php/7.3/apache2/conf.d/20-uploadlimit.ini
+    echo "post_max_size = 300M" >> /etc/php/7.3/apache2/conf.d/20-uploadlimit.ini
+    echo "done (OS Buster)"
+elif [ -d "/etc/php/7.4/" ]; then
+    echo "upload_max_filesize = 300M" > /etc/php/7.4/apache2/conf.d/20-uploadlimit.ini
+    echo "post_max_size = 300M" >> /etc/php/7.4/apache2/conf.d/20-uploadlimit.ini
+    echo "done (OS Bullseye)"
+elif [ -d "/etc/php/8.2/" ]; then
+    echo "upload_max_filesize = 300M" > /etc/php/8.2/apache2/conf.d/20-uploadlimit.ini
+    echo "post_max_size = 300M" >> /etc/php/8.2/apache2/conf.d/20-uploadlimit.ini
+    echo "done (OS Bookworm)"
+elif [ -d "/etc/php/8.3/" ]; then
+    echo "upload_max_filesize = 300M" > /etc/php/8.3/apache2/conf.d/20-uploadlimit.ini
+    echo "post_max_size = 300M" >> /etc/php/8.3/apache2/conf.d/20-uploadlimit.ini
+    echo "done (OS Trixie)"
+elif [ -d "/etc/php/8.4/" ]; then
+    echo "upload_max_filesize = 300M" > /etc/php/8.4/apache2/conf.d/20-uploadlimit.ini
+    echo "post_max_size = 300M" >> /etc/php/8.4/apache2/conf.d/20-uploadlimit.ini
+    echo "done (OS Sid or later)"
+else
+    echo "no supported PHP version found, skipping upload limit configuration"
+fi
+echo -n "enabling apache ssl module..."
+a2enmod ssl
+a2enmod proxy_wstunnel
+a2dissite default-ssl 2>/dev/null || true
+cp "${OPENWBBASEDIR}/data/config/apache/apache-openwb-ssl.conf" /etc/apache2/sites-available/
+a2ensite apache-openwb-ssl
+echo "done"
+echo -n "restarting apache..."
+systemctl restart apache2
+echo "done"
+
+# Setze USE_VENV basierend auf der Debian-Version
+if [[ "$DEBIAN_VERSION" == "12" || "$DEBIAN_VERSION" == "13" || "$DEBIAN_VERSION" == "unstable" ]]; then
+    USE_VENV=true
+else
+    USE_VENV=false
 fi
 
 # Setze PYTHON_EXEC und PIP_EXEC
@@ -142,149 +264,22 @@ else
     PIP_EXEC="/usr/bin/pip3"
 fi
 
-echo "Erstelle Gruppe $OPENWB_GROUP"
-# Macht nichts, wenn die Gruppe bereits existiert
-/usr/sbin/groupadd "$OPENWB_GROUP"
-echo "Fertig"
-
-echo "Erstelle Benutzer $OPENWB_USER"
-# Macht nichts, wenn der Benutzer bereits existiert
-/usr/sbin/useradd "$OPENWB_USER" -g "$OPENWB_GROUP" --create-home
-echo "Fertig"
-
-# Sudo-Rechte für den Benutzer hinzufügen
-echo "$OPENWB_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/openwb
-chmod 440 /etc/sudoers.d/openwb
-echo "Fertig"
-
-echo "Prüfe initialen Git-Clone..."
-if [ ! -d "${OPENWBBASEDIR}/web" ]; then
-    mkdir -p "$OPENWBBASEDIR"
-    chown "$OPENWB_USER:$OPENWB_GROUP" "$OPENWBBASEDIR"
-    sudo -u "$OPENWB_USER" git clone https://github.com/Xerolux/OpenWB2-Bookworm-Trixie.git --branch master "$OPENWBBASEDIR"
-    echo "Git aus dem Benutzer-Repository geklont"
-else
-    echo "OK"
-fi
-
-echo -n "Prüfe Ramdisk... "
-if grep -Fq "tmpfs ${OPENWBBASEDIR}/ramdisk" /etc/fstab; then
-    echo "OK"
-else
-    mkdir -p "${OPENWBBASEDIR}/ramdisk"
-    sudo tee -a "/etc/fstab" <"${OPENWBBASEDIR}/data/config/ramdisk_config.txt" >/dev/null
-    mount -a
-    echo "Erstellt"
-fi
-
-echo -n "Prüfe Crontab... "
-if [ ! -f /etc/cron.d/openwb ]; then
-    cp "${OPENWBBASEDIR}/data/config/openwb.cron" /etc/cron.d/openwb
-    echo "Installiert"
-else
-    echo "OK"
-fi
-
-# Mosquitto-Konfiguration
-echo "Aktualisiere Mosquitto-Konfigurationsdatei"
-systemctl stop mosquitto
-sleep 2
-cp -a "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto.conf" /etc/mosquitto/mosquitto.conf
-mkdir -p /etc/mosquitto/conf.d
-cp "${OPENWBBASEDIR}/data/config/mosquitto/openwb.conf" /etc/mosquitto/conf.d/openwb.conf
-cp "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto.acl" /etc/mosquitto/mosquitto.acl
-
-mkdir -p /etc/mosquitto/certs
-sudo cp /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/mosquitto/certs/openwb.pem
-sudo cp /etc/ssl/private/ssl-cert-snakeoil.key /etc/mosquitto/certs/openwb.key
-sudo chgrp mosquitto /etc/mosquitto/certs/openwb.key
-systemctl start mosquitto
-
-# Mosquitto_local Instanz
-if [ ! -f /etc/init.d/mosquitto_local ]; then
-    echo "Richte lokale Mosquitto-Instanz ein"
-    install -d -m 0755 -o root -g root /etc/mosquitto/conf_local.d/
-    install -d -m 0755 -o mosquitto -g root /var/lib/mosquitto_local
-    cp "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto_local_init" /etc/init.d/mosquitto_local
-    chown root:root /etc/init.d/mosquitto_local
-    chmod 755 /etc/init.d/mosquitto_local
-    systemctl daemon-reload
-    systemctl enable mosquitto_local
-else
-    systemctl stop mosquitto_local
-    sleep 2
-fi
-cp -a "${OPENWBBASEDIR}/data/config/mosquitto/mosquitto_local.conf" /etc/mosquitto/mosquitto_local.conf
-cp -a "${OPENWBBASEDIR}/data/config/mosquitto/openwb_local.conf" /etc/mosquitto/conf_local.d/
-systemctl start mosquitto_local
-echo "Mosquitto fertig"
-
-# Apache-Konfiguration
-echo -n "Konfiguriere Apache..."
-cp "${OPENWBBASEDIR}/data/config/apache/000-default.conf" "/etc/apache2/sites-available/"
-cp "${OPENWBBASEDIR}/index.html" /var/www/html/index.html
-echo "Fertig"
-
-echo -n "Passe Upload-Limit an..."
-if [ -d "/etc/php/7.3/" ]; then
-    echo "upload_max_filesize = 300M" > /etc/php/7.3/apache2/conf.d/20-uploadlimit.ini
-    echo "post_max_size = 300M" >> /etc/php/7.3/apache2/conf.d/20-uploadlimit.ini
-    echo "Fertig (PHP 7.3 - OS Buster)"
-elif [ -d "/etc/php/7.4/" ]; then
-    echo "upload_max_filesize = 300M" > /etc/php/7.4/apache2/conf.d/20-uploadlimit.ini
-    echo "post_max_size = 300M" >> /etc/php/7.4/apache2/conf.d/20-uploadlimit.ini
-    echo "Fertig (PHP 7.4 - OS Bullseye)"
-elif [ -d "/etc/php/8.2/" ]; then
-    echo "upload_max_filesize = 300M" > /etc/php/8.2/apache2/conf.d/20-uploadlimit.ini
-    echo "post_max_size = 300M" >> /etc/php/8.2/apache2/conf.d/20-uploadlimit.ini
-    echo "Fertig (PHP 8.2 - OS Bookworm)"
-elif [ -d "/etc/php/8.3/" ]; then
-    echo "upload_max_filesize = 300M" > /etc/php/8.3/apache2/conf.d/20-uploadlimit.ini
-    echo "post_max_size = 300M" >> /etc/php/8.3/apache2/conf.d/20-uploadlimit.ini
-    echo "Fertig (PHP 8.3 - OS Trixie)"
-elif [ -d "/etc/php/8.4/" ]; then
-    echo "upload_max_filesize = 300M" > /etc/php/8.4/apache2/conf.d/20-uploadlimit.ini
-    echo "post_max_size = 300M" >> /etc/php/8.4/apache2/conf.d/20-uploadlimit.ini
-    echo "Fertig (PHP 8.4)"
-else
-    echo "Keine unterstützte PHP-Version gefunden, überspringe Upload-Limit-Konfiguration"
-fi
-
-echo -n "Aktiviere Apache SSL-Modul..."
-a2enmod ssl
-a2enmod proxy_wstunnel
-a2dissite default-ssl 2>/dev/null || true
-cp "${OPENWBBASEDIR}/data/config/apache/apache-openwb-ssl.conf" /etc/apache2/sites-available/
-a2ensite apache-openwb-ssl
-echo "Fertig"
-
-echo -n "Starte Apache neu..."
-systemctl restart apache2
-echo "Fertig"
-
-# Python-Anforderungen installieren
-echo "Aktualisiere pip..."
+echo "installing python requirements..."
 if $USE_VENV; then
     sudo -u "$OPENWB_USER" "$PIP_EXEC" install --upgrade pip
-else
-    sudo -u "$OPENWB_USER" "$PIP_EXEC" install --user --upgrade pip
-fi
-
-echo "Installiere Python-Abhängigkeiten..."
-# Prüfe, ob requirements.txt existiert
-if [ ! -f "${OPENWBBASEDIR}/requirements.txt" ]; then
-    echo "Fehler: requirements.txt nicht gefunden in ${OPENWBBASEDIR}"
-    exit 1
-fi
-
-if $USE_VENV; then
     sudo -u "$OPENWB_USER" "$PIP_EXEC" install -r "${OPENWBBASEDIR}/requirements.txt"
 else
+    sudo -u "$OPENWB_USER" "$PIP_EXEC" install --user --upgrade pip
     sudo -u "$OPENWB_USER" "$PIP_EXEC" install --user -r "${OPENWBBASEDIR}/requirements.txt"
 fi
 
-echo "Installiere openWB2-Systemdienst..."
-# Dienstdatei mit der passenden Python-Version aktualisieren
+# Prüfe, ob die Installation erfolgreich war
+if [ $? -ne 0 ]; then
+    echo "Fehler bei der Installation der Python-Abhängigkeiten"
+    exit 1
+fi
+
+echo "installing openwb2 system service..."
 sed -i "s|ExecStart=.*|ExecStart=$PYTHON_EXEC -m openWB.run|" "${OPENWBBASEDIR}/data/config/openwb2.service"
 if ! $USE_VENV; then
     PYTHON_MAJOR_MINOR=$("$PYTHON_EXEC" --version | cut -d' ' -f2 | cut -d'.' -f1,2)
@@ -294,8 +289,7 @@ ln -sf "${OPENWBBASEDIR}/data/config/openwb2.service" /etc/systemd/system/openwb
 systemctl daemon-reload
 systemctl enable openwb2
 
-echo "Installiere openWB2-Remote-Support-Dienst..."
-# Dienstdatei mit der passenden Python-Version aktualisieren
+echo "installing openwb2 remote support service..."
 sed -i "s|ExecStart=.*python|ExecStart=$PYTHON_EXEC|" "${OPENWBBASEDIR}/data/config/openwbRemoteSupport.service"
 if ! $USE_VENV; then
     sed -i "/ExecStart=/i Environment=\"PYTHONPATH=/home/$OPENWB_USER/.local/lib/python${PYTHON_MAJOR_MINOR}/site-packages\"" "${OPENWBBASEDIR}/data/config/openwbRemoteSupport.service"
@@ -305,8 +299,8 @@ systemctl daemon-reload
 systemctl enable openwbRemoteSupport
 systemctl start openwbRemoteSupport
 
-echo "Installation abgeschlossen, starte jetzt den openWB2-Dienst..."
+echo "installation finished, now starting openwb2.service..."
 systemctl start openwb2
 
-echo "Alles fertig!"
-echo "Falls Sie diese Installation für Entwicklungsarbeiten nutzen möchten, setzen Sie ein Passwort für den Benutzer 'openwb' mit: sudo passwd openwb"
+echo "all done"
+echo "if you want to use this installation for development, add a password for user 'openwb' with: sudo passwd openwb"
